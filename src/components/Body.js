14f49c957074a40df01b93795afcbbc8af2ea625
Svetlozar Georgiev <svetlozar@Svetlozars-MacBook-Pro.local>
2021-10-27 03:25:37 +0100

    University of Southampton Computer Science Notes

---
+++ src/components/Body.js
@@ -0,0 +1,338 @@
+import React from "react";
+import logo from "./logo.png";
+
+const body = () => {
+  return (
+    <div className="main-page">
+      <h1>List of Computer Science modules in the 2021-2022 session.</h1>
+      <img className="logo-img" src={logo} alt="" />
+      <div className="modules">
+        <h2>Computer Science Year 2 Modules</h2>
+        <div className="modules-wrapper">
+          <div className="module">
+            <p className="id">COMP2207</p>
+            <i className="fas fa-wifi"></i>
+            <p className="name">Distributed Systems and Networks</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2207/38696/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/student/wiki/index.php/COMP2207"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Wiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/noteswiki/w/COMP2207"
+              target="_blank"
+              rel="noreferrer"
+            >
+              NotesWiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/notes/comp2207/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a class="disabled" href="#" target="_blank" rel="noreferrer">
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2208</p>
+            <i className="fas fa-robot"></i>
+            <p className="name">Intelligent Systems</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2208/31030/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/noteswiki/w/COMP2208"
+              target="_blank"
+              rel="noreferrer"
+            >
+              NotesWiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/notes/comp2208/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a
+              href="https://blackboard.soton.ac.uk/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_197774_1&handle=announcements_entry&mode=view"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2209</p>
+            <i className="fas fa-code"></i>
+            <p className="name">Programming III</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2209/31031/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/student/wiki/index.php/COMP2209"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Wiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/notes/comp2209/21-22/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a
+              href="https://blackboard.soton.ac.uk/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_197775_1&handle=announcements_entry&mode=view"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2210</p>
+            <i className="fas fa-laptop-code"></i>
+            <p className="name">Theory of Computing</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2210/31032/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/student/wiki/index.php/COMP2210"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Wiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/notes/comp2210/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a
+              href="https://blackboard.soton.ac.uk/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_197109_1&handle=announcements_entry&mode=view"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2211</p>
+            <i className="fas fa-users"></i>
+            <p className="name">Software Engineering Group Project</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2211/31033/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/student/wiki/index.php/COMP2211"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Wiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/noteswiki/w/COMP2211"
+              target="_blank"
+              rel="noreferrer"
+            >
+              NotesWiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/noteswiki/w/COMP2211_Notes"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a class="disabled" href="#" target="_blank" rel="noreferrer">
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2212</p>
+            <i className="fas fa-language"></i>
+            <p className="name">Programming Language Concepts</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2212/31034/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/notes/comp2212/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a class="disabled" href="#" target="_blank" rel="noreferrer">
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2213</p>
+            <i className="fas fa-palette"></i>
+            <p className="name">Interaction Design</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2213/38697/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/student/wiki/index.php/COMP2213"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Wiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/noteswiki/w/COMP2213"
+              target="_blank"
+              rel="noreferrer"
+            >
+              NotesWiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/notes/comp2213/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a
+              href="https://blackboard.soton.ac.uk/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_197111_1&handle=announcements_entry&mode=view"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2214</p>
+            <i className="fas fa-edit"></i>
+            <p className="name">Advanced Software Modelling and Design</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2214/31036/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/student/wiki/index.php/COMP2214"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Wiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/noteswiki/w/COMP2214"
+              target="_blank"
+              rel="noreferrer"
+            >
+              NotesWiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/notes/comp2214/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a class="disabled" href="#" target="_blank" rel="noreferrer">
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2215</p>
+            <i className="fas fa-network-wired"></i>
+            <p className="name">Computer Systems II</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2215/31037/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/student/wiki/index.php/COMP2215"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Wiki
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/notes/comp2215/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Notes
+            </a>
+            <a class="disabled" href="#" target="_blank" rel="noreferrer">
+              Blackboard
+            </a>
+          </div>
+          <div className="module">
+            <p className="id">COMP2216</p>
+            <i className="fas fa-user-shield"></i>
+            <p className="name">Principles of Cyber Security</p>
+            <a
+              href="https://secure.ecs.soton.ac.uk/module/2122/COMP2216/36507/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage
+            </a>
+            <a
+              href="https://secure.ecs.soton.ac.uk/noteswiki/w/COMP2216"
+              target="_blank"
+              rel="noreferrer"
+            >
+              NotesWiki
+            </a>
+            <a class="disabled" href="#" target="_blank" rel="noreferrer">
+              Blackboard
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default body;
